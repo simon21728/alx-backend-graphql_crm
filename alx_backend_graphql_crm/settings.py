dfd2aadeacf70722a4e4051a7c283dd4fe183168
Simon <simonbayu2211@exgmail.com>
2025-10-17 10:19:10 +0300

    Set Up GraphQL Endpoint

--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crm',
-    'graphene-django',
+     "crm",
+     "graphene_django",
+
     'django_filters',
 ]
 
