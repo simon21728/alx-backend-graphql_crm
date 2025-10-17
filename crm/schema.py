2c9e06f45685fb22692992a6673b9f8072af4a95
Simon <simonbayu2211@exgmail.com>
2025-10-17 11:14:13 +0300

    Task 1: Build and Seed a CRM Database with GraphQL Integration

--- crm/schema.py
+++ crm/schema.py
@@ -128,13 +128,20 @@ class CreateOrder(graphene.Mutation):
         return CreateOrder(order=order)
 
 # ---------------- Root Mutation ----------------
+class Query(graphene.ObjectType):
+    all_customers = graphene.List(CustomerType)
 
+    def resolve_all_customers(root, info):
+        return Customer.objects.all()
+
+# Mutation
 class Mutation(graphene.ObjectType):
     create_customer = CreateCustomer.Field()
     bulk_create_customers = BulkCreateCustomers.Field()
     create_product = CreateProduct.Field()
     create_order = CreateOrder.Field()
 
-class CRMQuery(graphene.ObjectType):
-    # Placeholder for future CRM queries
-    pass
+
+
+
+
